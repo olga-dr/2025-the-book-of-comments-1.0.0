e9b117137512e788da521b628052998f34aea9b8
olga-dr <143840335+olga-dr@users.noreply.github.com>
2025-02-24 14:31:36 -0500

    Tests (#2)
    * test optimization + hooks
    
    * test optimization + hooks

---
+++ src/_tests/gqlRequest.js
@@ -0,0 +1,11 @@
+
+const request = require ('supertest')
+const url = 'http://localhost:5000/'
+
+function gqlRequest(postData){
+    return request( url)
+           .post('/')
+           .send(postData)   
+    }
+
+    module.exports =gqlRequest
